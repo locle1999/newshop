fb768547911d61ae45fc2009932ac744c2bcec8b
locle99 <lephanphuoclocev@gmail.com>
2021-12-22 18:02:46 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import FromLogin from './components/FromLogin';
+import ILayoutProps from './components/ILayoutProps';
+const App = () => {
+
+
+  return (
+
+
+    <>
+
+      <FromLogin />
+
+    </>
+
+  );
+};
+
+
+
+export default App;
